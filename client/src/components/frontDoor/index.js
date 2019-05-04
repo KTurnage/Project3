90d611226c6ea0f9680efcd9b44c4217104a93da
KTurnage <kristaturnage314@gmail.com>
2019-05-04 11:25:49 -0600

    fronDoor index.js test success

--- client/src/components/frontDoor/index.js
+++ client/src/components/frontDoor/index.js
@@ -1,2 +1,13 @@
 import React from 'react'
 
+function FrontDoor () {
+    return (
+        <div
+            style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+            >
+            testing
+        </div>
+    )
+}
+
+export default FrontDoor;

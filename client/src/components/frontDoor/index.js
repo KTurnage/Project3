02de0699b13f4666a79f8775bf8190c3163cedf7
KTurnage <kristaturnage314@gmail.com>
2019-05-05 10:18:11 -0600

    puling new code

--- client/src/components/frontDoor/index.js
+++ client/src/components/frontDoor/index.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { Component} from 'react';
+import ReusableButton from './';
+import Room from './';
 
-function FrontDoor () {
-    return (
-        <div
-            style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-            >
-            testing
-        </div>
+class FrontDoor extends Component {
+    onClick() {
+        console.log("Button was clicked");
+      }
+    
+      render() {
+        return(
+          /** First Customized Component **/
+          <Room
+            cardColClass={"col-sm-3"}
+            cardBorder={"card border-default mx-1"}
+            cardTitle={"First Card"}
+            cardText={"Sample Card 1" }
+        > 
+        <ReusableButton className={"btn btn-primary"} onClick={this.onClick} >
+          Button Person
+        </ReusableButton>
+        <ReusableButton className={"btn btn-primary"} onClick={this.onClick} >
+          Button Activity1
+        </ReusableButton>
+        <ReusableButton className={"btn btn-primary"} onClick={this.onClick} >
+          Button Activity2
+        </ReusableButton>
+        <ReusableButton className={"btn btn-primary"} onClick={this.onClick} >
+          Button Heroku
+        </ReusableButton>
+        <ReusableButton className={"btn btn-primary"} onClick={this.onClick} >
+          Button Return to Hallway
+        </ReusableButton>
+        </Room>
+
+
+
+    // render() {
+    //     return (
+    //         <div
+    //         style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+    //         >
+    //         testing
+    //     </div>
     )
+}
 }
 
 export default FrontDoor;
